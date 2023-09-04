677257bb84f217eafd978c55056b2919eb1cd876
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-09-03 22:18:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/shoes.json').then((Response) => Response.json()).then(setData);
+  }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <div className="navbar">
+          <ul className="navLinks">
+            <li><i><ion-icon name="home"></ion-icon></i><a href="index.html">Home</a></li>
+            <li><i><ion-icon name="pricetag"></ion-icon></i><a href="index.html">Promoções</a></li>
+            <li><i><ion-icon name="information-circle"></ion-icon></i><a href="index.html">Sobre</a></li>
+          </ul>
+        </div>
+        <a className="button-login" href="index.html"><button>Login</button></a>
       </header>
+        <div className='logo'>
+          <img src="/static/static/images/logo.png" alt="KeepCompre Logo"/>
+        </div>
+
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image} = item
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name}/>
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">{oldPrice}</span>
+                <span className="price">{price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img src="/static/static/images/216151_right_chevron_icon.png" alt="Button Left"/>
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="/static/static/images/216151_right_chevron_icon.png" alt="Button Right"/>
+        </button>
+      </div>
     </div>
   );
 }
